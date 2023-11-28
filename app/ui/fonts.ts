7889f5bb4ab913d36ee7506ba88a18cc576529a1
Thomas.lerr <yotzapon@gmail.com>
2023-11-28 21:19:38 +0700

    feature: first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Lora, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lora = Lora({subsets: ['latin'], weight: ['400', '700']});
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
